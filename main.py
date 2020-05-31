7952d59b08f0c39cfd7d06316f06b8d227bcfe9b
Yiming Zhou <yimingzhou0416@gmail.com>
2020-05-30 17:57:58 -0700

    add both train test curves

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import argparse
 import torchvision
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', default='mnist', type=str, choices=['mnist', 'mnist_small', 'regression'])
+parser.add_argument('--dataset', default='mnist', type=str, choices=['cifar10', 'mnist', 'mnist_small', 'regression'])
 parser.add_argument('--optimizer', default='LM', type=str, choices=['LM', 'SGD', 'Adam', 'HF', 'KFAC'])
 parser.add_argument('--net_type', default='cnn', type=str, choices=['cnn', 'mlp'])
 parser.add_argument('--epoch_num', default=1, type=int)
-parser.add_argument('--device', default=2, type=int)
+parser.add_argument('--device', default=0, type=int)
 args = parser.parse_args()
 
 device = torch.device('cuda:' + str(args.device))
@@ -36,7 +36,7 @@ elif args.net_type == 'mlp':
     model = MLP().to(device)
 
 if args.optimizer == 'LM':
-    optimizer = LM(model.parameters(), lr=0.5, alpha=1)
+    optimizer = LM(model.parameters(), lr=0.5, alpha=10)
 elif args.optimizer == 'SGD':
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
 elif args.optimizer == 'Adam':
@@ -50,10 +50,14 @@ print (id)
 
 print ('-------------------training---------------------------')
 train_loss = []
+test_loss = []
+train_acc = []
 test_acc = []
 for epoch in range(args.epoch_num):
     for idx, (inputs, targets) in enumerate (trainloader):
+        print ('iter:{}'.format(idx + 1))
         inputs, targets = inputs.to(device), targets.to(device)
+        N = inputs.shape[0]
         if args.optimizer=='LM':
             def closure(sample=True):
                 N = inputs.shape[0]
@@ -70,10 +74,15 @@ for epoch in range(args.epoch_num):
                 else:
                     outputs = model(inputs)
                     loss = criterion(outputs, targets)
-                    return loss
+                    return outputs, loss
 
-            record_loss = optimizer.step(closure)
+            outputs, record_loss = optimizer.step(closure)
+            correct = torch.sum(torch.argmax(outputs,1) == targets).item()
+            
+            train_acc.append(correct/N)
             train_loss.append(record_loss)
+            if idx == 50:
+                break        
 
         elif args.optimizer == 'Adam' or args.optimizer == 'SGD':
             optimizer.zero_grad()
@@ -81,25 +90,29 @@ for epoch in range(args.epoch_num):
             loss = criterion(outputs,targets)
             loss.backward()
             optimizer.step()
+            correct = torch.sum(torch.argmax(outputs,1) == targets).item()
             train_loss.append(loss.item())
-        
-        if idx == 100:
-            break
+            train_acc.append(correct/N)
+
+            if idx == 50:
+                break
 
         print ('testing...')
         total, correct = 0, 0
+        running_loss = 0.0
         for idx, (inputs, targets) in enumerate(testloader):    
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = model(inputs)
+            loss = F.cross_entropy(outputs, targets)
+            running_loss += loss.item()
             correct += torch.sum(torch.argmax(outputs,1) == targets).item()
             total += inputs.shape[0]
-        print (correct/total)
         test_acc.append(correct/total)
+        test_loss.append(running_loss/(idx+1))
   
-
     np.save('./loss_acc_timing/' + id + '_train_loss.npy', np.asarray(train_loss))
+    np.save('./loss_acc_timing/' + id + '_test_loss.npy', np.asarray(test_loss))
+    np.save('./loss_acc_timing/' + id + '_train_acc.npy', np.asarray(train_acc))
     np.save('./loss_acc_timing/' + id + '_test_acc.npy', np.asarray(test_acc))
-
-
 
 
